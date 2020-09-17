a5bd5e16f95dc04cf6532980ddd4da2570c1145f
adiazgargiulo <adiaz@atixlabs.com>
2020-09-17 12:03:14 -0300

    fix: notification

--- app/App.js
+++ app/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Provider} from 'react-redux';
 import firebase from '@react-native-firebase/app';
 import {Root} from 'native-base';
@@ -6,55 +6,34 @@ import {MenuProvider} from 'react-native-popup-menu';
 import store from './src/store/store';
 import Nav from './src/components/components/navigation/Nav';
 import checkPermission from './src/services/notifications';
-import {
-  enabledNotifications,
-  notificationListener,
-  notificationOpen,
-} from './src/services/notifications/bgActions';
+
 import NavigationService from './src/services/navigation';
+import messaging from '@react-native-firebase/messaging';
 
-class App extends React.Component {
-  async componentDidMount() {
-    try {
-      await checkPermission();
+const App = () => {
 
-      // if (enabledNotifications) {
-      //   // NOTIFICATIONS LISTENERS
-      //   this.onNotificationListener = notificationListener(this.navigator);
-      //   this.notificationOpenListener = firebase
-      //     .messaging()
-      //     .onNotificationOpened(({notification: {data}}) =>
-      //       notificationOpen(this.navigator, data),
-      //     );
-      //   await notificationOpen(this.navigator);
-      // }
-      // We do not care about errors here
-      // eslint-disable-next-line no-empty
-    } catch (error) {}
-  }
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
 
-  componentWillUnmount() {
-    // Remove listeners to prevent double listeners
-    // this.onNotificationListener();
-    // this.notificationOpenListener();
-  }
+    return unsubscribe;
+  }, []);
 
-  render() {
-    return (
-      <Provider store={store}>
-        <MenuProvider>
-          <Root>
-            <Nav
-              ref={nav => {
-                this.navigator = nav;
-                NavigationService.setTopLevelNavigator(nav);
-              }}
-            />
-          </Root>
-        </MenuProvider>
-      </Provider>
-    );
-  }
+  return (
+    <Provider store={store}>
+      <MenuProvider>
+        <Root>
+          <Nav
+            ref={nav => {
+              this.navigator = nav;
+              NavigationService.setTopLevelNavigator(nav);
+            }}
+          />
+        </Root>
+      </MenuProvider>
+    </Provider>
+  );
 }
 
 export default App;
