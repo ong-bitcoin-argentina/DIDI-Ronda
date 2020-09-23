5ac0a9911ac1e6687cabf3564bc48e54e478f088
fsawadyatix <fsawady@atixlabs.com>
2020-09-23 14:29:24 -0300

    fix: wrong login test

--- api/helpers/validators.js
+++ api/helpers/validators.js
@@ -55,8 +55,7 @@ exports.validation = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log({ errors: errors.array()});
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   next();
