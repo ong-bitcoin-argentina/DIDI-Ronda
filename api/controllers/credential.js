fbaae3e4b1672e8de08846183b94d728ed86a931
Nicolas Laitan <nlaitan@atixlabs.com>
2020-10-01 13:03:33 -0300

    fix: remove unnecesary

--- api/controllers/credential.js
+++ api/controllers/credential.js
@@ -4,20 +4,6 @@ const { generic } = require("../helpers/errorHandler");
 const round_services = require("../services/round");
 const credential_services = require("../services/credential");
 
-exports.emmitCompletedRounds = async (req, res) => {
-	try {
-		const rounds = await round_services.completed();
-
-		return round && round.error
-			? res.status(200).jsonp({ error: round.error })
-			: res.status(200).jsonp(rounds);
-	} catch (err) {
-		return err.name === "customError"
-			? generic(res, err.message)
-			: generic(res, "");
-	}
-};
-
 exports.emmitRoundParticipantsById = async (req, res) => {
 	try {
 		const round = await round_services.byId(req);
