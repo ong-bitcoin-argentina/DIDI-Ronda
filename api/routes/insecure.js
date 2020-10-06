3fd5d9555e5de9d1fd52b4e80321a0560d8847c4
Nicolas Laitan <nlaitan@atixlabs.com>
2020-10-06 12:51:14 -0300

    fix: get user rounds

--- api/routes/insecure.js
+++ api/routes/insecure.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { validation } = require("../helpers/validators");
+const { validation, username } = require("../helpers/validators");
 
 // CONTROLLER
 const round_controller = require("../controllers/round");
+const user_controller = require("../controllers/user");
 
 // CREATE ROUTES ON ROUTER
 
 // get round by id (this helps to get virtual fields)
 router.get("/round/:roundId", [], validation, round_controller.byId);
+
+router.get("/rounds", [username], validation, user_controller.roundsOfUser);
 
 // finish round with simulated fields
 router.post(
