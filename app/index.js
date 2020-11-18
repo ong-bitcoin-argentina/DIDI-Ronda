cb3639bc7bd340dd68bf3f9e2e38c34fd23928b4
Juan Ignacio Martel <jmartel@atixlabs.com>
2020-11-18 10:53:13 -0300

    instalacion de libreria y primeros pasos

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 import { AppRegistry, YellowBox } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
-import messaging from '@react-native-firebase/messaging';
+import messaging from "@react-native-firebase/messaging";
 import {
   enabledNotifications,
   notificationListener,
   notificationOpen,
-} from './src/services/notifications/bgActions';
+} from "./src/services/notifications/bgActions";
+import PushNotification from "react-native-push-notification";
 
 global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
 global.FormData = global.originalFormData || global.FormData;
@@ -26,8 +27,50 @@ YellowBox.ignoreWarnings([
 // ./FOR react-native-swipeout LIBRARY
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('Message handled in the background!', remoteMessage);
+  console.log("Message handled in the background!", remoteMessage);
   notificationOpen(remoteMessage);
+});
+
+PushNotification.configure({
+  // (optional) Called when Token is generated (iOS and Android)
+  onRegister: function(token) {
+    console.log("TOKEN:", token);
+  },
+
+  // (required) Called when a remote is received or opened, or local notification is opened
+  onNotification: function(notification) {
+    console.log("NOTIFICATION:", notification);
+
+    // process the notification
+  },
+
+  // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+  onAction: function(notification) {
+    console.log("ACTION:", notification.action);
+    console.log("NOTIFICATION:", notification);
+
+    // process the action
+  },
+
+  // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+  onRegistrationError: function(err) {
+    console.error(err.message, err);
+  },
+
+  // Should the initial notification be popped automatically
+  // default: true
+  popInitialNotification: true,
+
+  senderID: "323695863108",
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: true,
 });
 
 AppRegistry.registerComponent(appName, () => App);
