f3543ec0172348c5d209e74aee461000135e1790
Nicolas Laitan <nlaitan@atixlabs.com>
2020-11-24 15:56:02 -0300

    feat: return number of unreaded notifications
    Signed-off-by: Nicolas Laitan <nlaitan@atixlabs.com>

--- api/services/notification.js
+++ api/services/notification.js
@@ -2,6 +2,30 @@ const user_manager = require("../managers/user");
 const notifications_manager = require("../managers/notification");
 const { customError } = require("../helpers/errorHandler");
 
+exports.getNotifications = async req => {
+  const { username } = req.body;
+  const { page, limit } = req.query;
+  const pageNumber = Number(page);
+  const limitNumber = Number(limit);
+
+  const user = await user_manager.byUsername(username);
+  if (!user) throw new customError("User don't exists");
+
+  const list = await notifications_manager.byUserId(
+    user._id,
+    pageNumber,
+    limitNumber
+  );
+
+  return {
+    items: list,
+    unreaded: list.filter(item => !item.viewedAt).length,
+    count: await notifications_manager.countByUserId(user._id),
+    page: pageNumber || 0,
+    limit: limitNumber || undefined
+  };
+};
+
 exports.markNotificationsAsViewed = async req => {
   const { username } = req.body;
   const user = await user_manager.byUsername(username);
