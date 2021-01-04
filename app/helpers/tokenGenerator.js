39ae6e2168316adb0111e252b31357e9284a4fae
Nicolas Laitan <nlaitan@atixlabs.com>
2021-01-04 11:50:27 -0300

    fix: remove duplicated folders and files
    Signed-off-by: Nicolas Laitan <nlaitan@atixlabs.com>

--- app/helpers/tokenGenerator.js
+++
@@ -1,7 +0,0 @@
-const crypto = require("crypto");
-
-//This generates a unique token to use wherever we want
-exports.generate = () => {
-const buff = crypto.randomBytes(4);
-return parseInt(buff.toString('hex'),16);
-}
--- app/managers/web3-client.js
+++
@@ -1,6 +0,0 @@
-const Web3Client = require("web3");
-const { WEB3_NODE_LOCATION } = process.env;
-
-exports.web3Client = new Web3Client(
-  Web3Client.givenProvider || WEB3_NODE_LOCATION
-);
