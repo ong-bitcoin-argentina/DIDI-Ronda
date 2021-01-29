acf89211fcc9221df936c89561fe55610fd1c0b3
Juan Ignacio Martel <jmartel@atixlabs.com>
2021-01-29 15:53:40 -0300

    fix: DIDIS-159 change ruffle roulette colors

--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -1,3 +1,18 @@
 export const notificationsCodes = {
   errorSC: "register-user-failed",
 };
+
+export const ruffleRouletteColors = [
+  "#417FD7",
+  "#69A7FE",
+  "#E03C7A",
+  "#F97EAD",
+  "#06CBB5",
+  "#5CE6D6",
+  "#6ECC62",
+  "#92ED86",
+  "#FFC500",
+  "#FFDE6D",
+  "#906ECD",
+  "#B89AED",
+];
