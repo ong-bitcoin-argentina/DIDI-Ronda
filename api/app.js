6a44150ce5332d6793015578888347ef3bc5c03c
Matias Oveja Smith <matias.ovejasmith@bitcoinargentina.org>
2021-10-21 00:30:02 -0300

    chore: Take Version from env variables

--- api/app.js
+++ api/app.js
@@ -8,13 +8,12 @@ const http = require("http");
 http.createServer(app);
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { version } = require("./package.json");
 const helmet = require("helmet");
 
 // Agenda
 const { agendaStart, permanentJob } = require("./jobs");
 
-const { PORT, MONGO_URI, ENVIRONMENT } = process.env;
+const { PORT, MONGO_URI, ENVIRONMENT, VERSION } = process.env;
 console.log(`MONGO_URI: ${MONGO_URI}`);
 
 // parse application/json
@@ -84,7 +83,7 @@ mongoose.connect(MONGO_URI,  {
     () => { 
       app.listen(PORT, () => {
         console.log(`------ LA RONDA API ------`);
-        console.log(`-   version ${version}   -`);
+        console.log(`-   version ${VERSION}   -`);
         console.log(`-   ENV: ${ENVIRONMENT}  -`);
         console.log(`--------------------------`);
         console.log(`Node server running on port ${PORT}`);
